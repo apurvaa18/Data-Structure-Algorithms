8d7e16f138945012f4d24a128e9b2150fc57d4e7
Apurva Thakre <157197850+apurvaa18@users.noreply.github.com>
2025-05-15 23:38:26 +0530

    Add files via upload

---
+++ Array/RotatedSortedArray.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class RotatedSortedArray {
+
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] >= nums[left]) {
+                if (nums[left] <= target && target <= nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if (nums[mid] <= target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        RotatedSortedArray solver = new RotatedSortedArray();
+
+        int[] nums = {4, 5, 6, 7, 0, 1, 2};
+        int target = 0;
+
+        int result = solver.search(nums, target);
+        if (result != -1) {
+            System.out.println("Target " + target + " found at index: " + result);
+        } else {
+            System.out.println("Target " + target + " not found in the array.");
+        }
+    }
+}
