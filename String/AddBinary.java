47d1d358562bb2a8aa46e4aaecf21c58ca0e346f
Apurva Thakre <157197850+apurvaa18@users.noreply.github.com>
2025-05-27 23:22:11 +0530

    Add files via upload

---
+++ String/AddBinary.java
@@ -0,0 +1,32 @@
+public class AddBinary {
+
+    public static String addBinary(String a, String b) {
+        char[] charArrA = a.toCharArray();
+        char[] charArrB = b.toCharArray();
+        StringBuilder sb = new StringBuilder();
+
+        int i = charArrA.length - 1, j = charArrB.length - 1;
+        int carry = 0;
+        while (i >= 0 || j >= 0 || carry > 0) {
+            int aNum = i >= 0 ? charArrA[i] - '0' : 0;
+            int bNum = j >= 0 ? charArrB[j] - '0' : 0;
+
+            int sum = aNum + bNum + carry;
+            int digit = sum % 2;
+            carry = sum / 2;
+            i--;
+            j--;
+
+            sb.append((char)(digit + '0'));
+        }
+
+        return sb.reverse().toString();
+    }
+
+    // Optional: main method for testing
+    public static void main(String[] args) {
+        String a = "1010";
+        String b = "1011";
+        System.out.println(addBinary(a, b)); // Output: "10101"
+    }
+}
