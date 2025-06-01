093122e0ef4693c9fb84c36d1f340e9c5c8f5d99
Apurva Thakre <157197850+apurvaa18@users.noreply.github.com>
2025-06-01 23:42:27 +0530

    Add files via upload

---
+++ Array/intersectionTwoArrays.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class intersectionTwoArrays {
+
+    public static int[] intersection(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums1) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        List<Integer> resultList = new ArrayList<>();
+        for (int num : nums2) {
+            if (map.containsKey(num)) {
+                resultList.add(num);
+                map.remove(num);
+            }
+        }
+
+        int[] result = new int[resultList.size()];
+        for (int i = 0; i < resultList.size(); i++) {
+            result[i] = resultList.get(i);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {1, 2, 2, 1};
+        int[] nums2 = {2, 2};
+
+        int[] result = intersection(nums1, nums2);
+
+        System.out.print("Intersection: ");
+        for (int num : result) {
+            System.out.print(num + " ");
+        }
+    }
+}
