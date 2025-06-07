43b3814dc6e8167c11497b38e2ab2643d11ec990
Apurva Thakre <157197850+apurvaa18@users.noreply.github.com>
2025-06-07 23:34:05 +0530

    Add files via upload

---
+++ Recursion/PowerOfFour.java
@@ -0,0 +1,8 @@
+public class PowerOfFour {
+    public boolean isPowerOfFour(int n) {
+        if (n <= 0) {
+            return false;
+        }
+        return Math.log(n) / Math.log(4) % 1 == 0;
+    }
+}
