1c70c96a19281bf47e8e3424a7e59c6f4e8e390a
Apurva Thakre <157197850+apurvaa18@users.noreply.github.com>
2025-06-10 23:16:27 +0530

    Add files via upload

---
+++ Hash Table/containsNearbyDuplicate.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class DuplicateChecker {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer, Integer> seen = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int val = nums[i];
+            if (seen.containsKey(val) && i - seen.get(val) <= k) {
+                return true;
+            }
+            seen.put(val, i);
+        }
+
+        return false;
+    }
+}
